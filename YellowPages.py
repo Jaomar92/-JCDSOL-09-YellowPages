2c3c8271d9b52e5cce7c8a25c38309156eb12c20
Jaomar92 <65635396+Jaomar92@users.noreply.github.com>
2023-02-13 00:04:41 +0700

    initialize database

--- YellowPages.py
+++ YellowPages.py
@@ -1,8 +1,49 @@
 #Student: Jeffey Aroun bin Omar
 
+#Initialization of DataBase
+yellowPages = [
+    {"A":
+     [
+      {'nama':'Arif Ikram', "hp":"019295488", 'Kota':"Jakarta Selatan", "zip":59000},
+      {'nama':'Alif Fitri', 'hp':"015024406", 'Kota': "Damansara", 'zip': 72999}
+      ]
+      },
+      {"B":[{"nama":"","hp":'','Kota':"","zip":''}]},
+      {"C":{}},
+      {"D":{}},
+      {"E":{}},
+      {"F":{}},
+      {"G":{}},
+      {"H":{}},
+      {"I":{}},
+      {"J":{}},
+      {"K":{}},
+      {"L":{}},
+      {"M":{}},
+      {"N":{}},
+      {"O":{}},
+      {"P":{}},
+      {"Q":{}},
+      {"R":{}},
+      {"S":{}},
+      {"T":{}},
+      {"U":{}},
+      {"V":{}},
+      {"W":{}},
+      {"X":{}},
+      {"Y":{}},
+      {"Z":{}},
+      {"#":{}},
+
+    ]
+
+
 def Greeting():
     print("Halo Selamat Datang ke Jeffrey punya Python Yello Pages Program! ")
     print("Bagai Mana saya bisa Bantu anda? ")
 
 
 Greeting()
+
+print(type(yellowPages[1]["B"]))
+
