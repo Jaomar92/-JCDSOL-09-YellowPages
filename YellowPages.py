fd675b76d22aa6eede663cbe7e274bcabac45901
Jaomar92 <65635396+Jaomar92@users.noreply.github.com>
2023-02-13 18:32:21 +0700

    Update Function done!

--- YellowPages.py
+++ YellowPages.py
@@ -34,40 +34,6 @@ def Menu():
     5. Keluar.
     ''')
 
-
-
-# Function to Add Contact
-
-def Add_contact():
-    state =True
-    while state:
-        # Take contact Details
-        contactName = input("Name of the person you would like to add: ").title()
-        # Get the first character of the name
-        N = contactName[0] if contactName[0].isalpha() else '#'
-        # If the key doesn't exist in the dictionary, add it
-        if N not in yellow_pages:
-            yellow_pages[N] = []
-        # Checks if user already exist
-        for i in yellow_pages[N]:
-            if (contactName == i['nama']):
-                print('\n')
-                print("!!!<---- User already exists ---->!!!!")
-                print('\n')
-                state = False
-                return state
-                    
-        contactNum = input("what is the contact number: ")
-        contactKota = input("contact Kota: ")
-        contactZip = input("Contact zip: ")
-        yellow_pages[N].append({'nama':contactName, 'hp':contactNum, 'kota':contactKota, 'zip':contactZip})
-        state = False
-    
-    
-# How to add an item to a dictionary
-
-
-
 # Function to organize the dictionary. 
 def organize_contact():
     print(' ')
@@ -86,30 +52,101 @@ def organize_contact():
             print('  ', j['nama'],'\n','|',"contact: ", j['hp']," "*(15 -len(j['hp'])), "|", "kota: ", j['kota']," "*(15 -len(j['kota'])), "|" ,"zip: ", j['zip']," "*(7 -len(j['zip'])), "|")
             print('--------------------------------------------------------------------------')
         print()
-    
-            
+
+
+# Function to Add Contact
+def Add_contact():
+    state =True
+    while state:
+        # Take contact Details
+        # first error check is name
+        contactName = input("Name of the person you would like to add: ").title()
+        # Get the first character of the name
+        # checks if N is an alphabet or something else.
+        N = contactName[0] if contactName[0].isalpha() else '#'
+        # checks if N is in the yellow_pages dict.
+        # if it is not it will create a new dict key.
+        if N not in yellow_pages:
+            yellow_pages[N] = []
+        # Checks if user already exist
+
+        for i in yellow_pages[N]:
+            if (contactName == i['nama']):
+                print('\n')
+                print("!!!<---- User already exists ---->!!!!")
+                print('\n')
+                state = False
+                return state
+        # continues if all is in order. 
+        contactNum = input("what is the contact number: ")
+        contactKota = input("contact Kota: ")
+        contactZip = input("Contact zip: ")
+        yellow_pages[N].append({'nama':contactName, 'hp':contactNum, 'kota':contactKota, 'zip':contactZip})
+        state = False
+# Update Contact
+
+
+def Update_contact():
+    organize_contact()
+    #Get user contact they wish to update.
+    contactName = input("Enter the name of the contact you would like to delete: ").title()
+    # Checks if its a alphabet or char.
+    N = contactName[0] if contactName[0].isalpha() else '#'
+    for i in yellow_pages[N]:
+        if (contactName == i['nama']):
+            i['hp'] = input("Enter the updated contact number: ")
+            i['kota'] = input("Enter the updated city: ")
+            i['zip'] = input("Enter the updated zip: ")
+            print("Contact has been updated successfully")
+            return
+    print('\n')
+    print("!!!<-----Contact not found----->!!!")
+    print('\n')
+
+# Delete contact
+def Delete_contact():
+    #prints current contact list for better reference
+    organize_contact()
+    # Ask user which contact they want to delete.
+    contactName = input("Enter the name of the contact you would like to delete: ").title()
+    # checks if the contact exsist.
+    N = contactName[0] if contactName[0].isalpha() else '#'
+    # iterates contact list to find a match
+    for i in yellow_pages[N]:
+        if (contactName == i['nama']):
+            yellow_pages[N].remove(i)
+            print("Contact has been deleted successfully")
+            return
+    print('\n')
+    print("!!!<-----Contact not found----->!!!")
+    print('\n')
 
 
 # organize_contact()
 Greeting()
 while True:
     Menu()
-    userOption = int(input(" Sila pilih nomur: "))
-    if (userOption == 1):
-        print("user picked: " + str(userOption))
+    userOption = input(" Sila pilih nomur: ")
+    userInput = int(userOption if userOption.isdigit() else 6)
+    if (userInput == 1):
+        print("Anda pilih: " + str(userOption))
         Add_contact()
-    elif (userOption == 2):
-        print("user picked: " + str(userOption))
-    elif (userOption == 3):
-        print("user picked: " + str(userOption))
-    elif (userOption == 4):
-        print("user picked: " + str(userOption))
+    elif (userInput == 2):
+        print("Anda pilih: " + str(userOption))
+        Update_contact()
+    elif (userInput == 3):
+        print("Anda pilih: " + str(userOption))
+        Delete_contact()
+    elif (userInput == 4):
+        print("Anda pilih: " + str(userOption))
         organize_contact()
-    elif (userOption == 5):
-        print("user picked: " + str(userOption))
+    elif (userInput == 5):
+        print("Anda pilih: " + str(userOption))
         break
     else:
-        print("Please input the proper prompt.")
+        print('\n')        
+        print("!!!<---Pilihan yang anda masukkan salah. Silakan coba lagi.--->!!! ")
+        print('\n')        
 # contactName = input("Name of the person you would like to add: ").title()
 # N = contactName[0]
 # for i in yellow_pages[N]:
