83dee3a96bb2cc7e9ec0816b02049d68356905e7
Jaomar92 <65635396+Jaomar92@users.noreply.github.com>
2023-02-17 11:17:14 +0700

    minor changes

--- YellowPages.py
+++ YellowPages.py
@@ -2,13 +2,13 @@
 
 #Initialization of DataBase
 yellow_pages = { 
-    "C":[{"nama":"Chandra", "hp":"6282127289952", "kota":'jakarta Barat', "zip":'132190'}],
-    "D":[{"nama":"Dita Claudia", "hp":"628111366828", "kota":'jakarta Selatan', "zip":'12190'}],
+    "C":[{"nama":"Chandra", "hp":"6282127289952", "kota":'Jakarta Barat', "zip":'132190'}],
+    "D":[{"nama":"Dita Claudia", "hp":"628111366828", "kota":'Jakarta Selatan', "zip":'12190'}],
     "F":[
-    {"nama":"Fuad", "hp":"6281482228265", "kota":'jakarta Selatan', "zip":'668258'},
-    {"nama":"Fatima", "hp":"6282242807286", "kota":'jakarta timur', "zip":'599872'},
+    {"nama":"Fuad", "hp":"6281482228265", "kota":'Jakarta Selatan', "zip":'668258'},
+    {"nama":"Fatima", "hp":"6282242807286", "kota":'Jakarta timur', "zip":'599872'},
     ],
-    "J":[{"nama":"Jeffrey Omar", "hp":"085219787939", "kota":'jakarta Selatan', "zip":'12190'}], 
+    "J":[{"nama":"Jeffrey Omar", "hp":"085219787939", "kota":'Jakarta Selatan', "zip":'12190'}], 
     "K":[{"nama":"Karina Farida", "hp":"6281515745925", "kota":'Depok', "zip":'294522'}],
    
     
@@ -157,7 +157,7 @@ def Add_contact():
                 return state
         # continues if all is in order. 
         contactNum = input("what is the contact number: ")
-        contactKota = input("contact Kota: ")
+        contactKota = input("contact Kota: ").title()
         contactZip = input("Contact zip: ")
         yellow_pages[N].append({'nama':contactName, 'hp':contactNum, 'kota':contactKota, 'zip':contactZip})
         state = False
